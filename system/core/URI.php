d38f669bea731b343cf76128328940d7b85bc4a2
Willy Yeh <willy@ecrowdmedia.com>
2024-12-06 13:20:44 +0800

    避開 ltrim() 不得傳 null 的限制

--- system/core/URI.php
+++ system/core/URI.php
@@ -639,7 +639,7 @@ class CI_URI {
 	 */
 	public function ruri_string()
 	{
-		return ltrim(load_class('Router', 'core')->directory, '/').implode('/', $this->rsegments);
+		return ltrim(load_class('Router', 'core')->directory ?? '', '/').implode('/', $this->rsegments);
 	}
 
 }
